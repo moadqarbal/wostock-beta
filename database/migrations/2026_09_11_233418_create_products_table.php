2ad4d334d944e3fc19e4c2e26cdd018c53f14384
moadqarbal <moadqarbal@gmail.com>
2026-09-12 00:00:20 +0100

    add migrations fields

---
+++ database/migrations/2026_09_11_233418_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
+
+            $table->foreignId('supplier_id')->constrained('suppliers')->restrictOnDelete();
+
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('stock_quantity')->default(0);
+            $table->unsignedInteger('minimum_stock')->default(0);
+            $table->string('image')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
